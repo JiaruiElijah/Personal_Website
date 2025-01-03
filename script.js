24b54668f1b133d6406f382fe768dd38aa581a96
JiaruiElijah <113058370+JiaruiElijah@users.noreply.github.com>
2025-01-02 21:57:12 -0500

    Push

---
+++ script.js
@@ -0,0 +1,13 @@
+bgzy.init([
+  "img/recur.png",
+  "img/zhuoyou.jpg",
+  "img/FRA.jpg",
+  "img/画画.jpg",
+  "img/wenzhang.jpg",
+  "img/mu.jpg",
+  "img/duizhan.jpg"
+],{
+  timeout:2000,
+  showTicker: true
+}
+);
